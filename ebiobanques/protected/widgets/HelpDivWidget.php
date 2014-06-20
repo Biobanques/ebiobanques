6cbab7a4d39d1d03e6ac6b67b3c1664fe9dc5464
nicolas@malservet.eu <nicolas@malservet.eu>
2014-06-20 10:16:38 +0200

    correct a end-div too much. add code comment in english.

--- ebiobanques/protected/widgets/HelpDivWidget.php
+++ ebiobanques/protected/widgets/HelpDivWidget.php
@@ -1,14 +1,11 @@
 <?php
 /**
  * HelpDivWidget class file.<br>
- * widget d 'affichage de div clicquable
+ * widget ti display a cliquable div showing a contextual help.
  *
  * @author Malservet Nicolas <nicolas.malservet@inserm.fr>
- * @copyright Copyright &copy; 2014 BioSoftware Factory
+ * @copyright Copyright &copy; 2014 Biobanques
  */
-
-
-
 class HelpDivWidget extends CWidget
 {
 	/**
@@ -35,13 +32,14 @@ class HelpDivWidget extends CWidget
 		echo "<a   style=\"margin: 0px 0px 0px 5px;\" onclick=\"toggleHelp('".$this->id."')\">";
 		echo CHtml::image(Yii::app()->request->baseUrl.'/images/help.png');
 		echo "</a>";
-		echo "</div>
+		echo "
 		<!-- div aide advanced search -->
 		<div id=\"".$this->id."\" style=\"display:none;margin-top: 5px;border:1px solid blueviolet;background: #eeeeee;padding:5px;\">
                     <img src=\"".Yii::app()->request->baseUrl.'/images/'."help.png\"/ style=\"margin-right: 5px;\">
 		<p style=\"display:inline;\">
 		".$this->text."
 		</p>
+                ooooo
 		</div>";
 	}
 }

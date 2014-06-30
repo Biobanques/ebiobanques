f2dda9e83909052cb6659dd34b14fe55bedb3d4d
nicolas@malservet.eu <nicolas@malservet.eu>
2014-06-30 15:39:22 +0200

    correct reference framework and delete dead comment

--- ebiobanques/protected/tests/bootstrap.php
+++ ebiobanques/protected/tests/bootstrap.php
@@ -6,13 +6,12 @@
  */
 //properties to load url db etc.
 include dirname(__FILE__) . '/../../CommonProperties.php';
-//echo "devurl".CommonProperties::$CONNECTION_STRING_DEV;
 //includes fichiers statiques à la racine
 require_once dirname(__FILE__) . '/../../CommonMailer.php';
 require_once dirname(__FILE__) . '/../../CommonTools.php';
 require_once dirname(__FILE__) . '/../../SmartResearcherTool.php';
 //yii include path
-$yiit = dirname(__FILE__) . '/../../yii-1.1.14/framework/yiit.php';
+$yiit = dirname(__FILE__) . '/../../yii-1.1.15/framework/yiit.php';
 $config = dirname(__FILE__) . '/../config/test.php';
 require_once ($yiit);
 Yii::createWebApplication($config);

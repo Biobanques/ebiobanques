326436e0efd466559356bf93e3ec0e92411fc4a4
nicolas@malservet.eu <nicolas@malservet.eu>
2015-01-08 11:11:25 +0100

    ignore + clean format

--- ebiobanques/protected/controllers/SiteController.php
+++ ebiobanques/protected/controllers/SiteController.php
@@ -88,7 +88,7 @@ class SiteController extends Controller
     public function actions() {
         $captcha = array(
             'class' => 'CaptchaExtendedAction',
-            'mode' => CaptchaExtendedAction::MODE_WORDS,
+            'mode' => CaptchaExtendedAction::MODE_WORDS,  
         );
         //ajout de fixed value si mode de dev
         if (CommonTools::isInDevMode()) {

dc3f18b45159077b8ae6f3ced68ce775620ad7b1
nicolas@malservet.eu <nicolas@malservet.eu>
2015-04-30 14:29:13 +0200

    add version 1.4

--- ebiobanques/protected/views/layouts/tpl_footer.php
+++ ebiobanques/protected/views/layouts/tpl_footer.php
@@ -1,6 +1,6 @@
 <footer>
     <div style="text-align:center;background-color: white;">
-        Copyright Inserm - Version 1.3 rev. <?php include 'revision_number.php' ?>- Project Biobanques <a href="http://www.biobanques.eu">www.biobanques.eu</a>
+        Copyright Inserm - Version 1.4 rev. <?php include 'revision_number.php' ?>- Project Biobanques <a href="http://www.biobanques.eu">www.biobanques.eu</a>
     </div>
 </footer>
 

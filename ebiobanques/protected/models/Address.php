dc705729e7347a2847f9725d30402456794ab3be
mpenicaud <matthieu.penicaud@inserm.fr>
2015-06-08 18:46:18 +0200

    fix test case error on biobank validation

--- ebiobanques/protected/models/Address.php
+++ ebiobanques/protected/models/Address.php
@@ -32,7 +32,7 @@ class Address extends EMongoSoftEmbeddedDocument
     // We may define rules for embedded document too
     public function rules() {
         return array(
-            array('street,city,zip,country', 'required', 'on' => 'create, update'),
+            array('street,city,zip,country', 'required', 'on' => 'insert, update'),
             array('street,city,zip,country', 'safe', 'on' => 'search'),
         );
     }

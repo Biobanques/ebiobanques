a487c7f236e4e6dafbf32ee1d12f3ea448719340
nicolas@malservet.eu <nicolas@malservet.eu>
2015-06-11 11:39:08 +0200

    run dev test suites delete lines related to 1.5 milestone update

--- tests_selenium/run_dev_test_suites.sh
+++ tests_selenium/run_dev_test_suites.sh
@@ -12,7 +12,7 @@ CURRENTPATH=$(pwd)
 #file to store localhost connection parameters like : --username ebiobanques --password ebiobanques --authenticationDatabase admin
 MONGO_CONNECTION_PARAMETERS=$EXTERNAL_DATA_PATH"/localhost_mongo_parameters.txt"
 #dump file
-DUMP_FILE=$EXTERNAL_DATA_PATH"dumps/dump_interopdb_15-04-23/interop/"
+DUMP_FILE=$EXTERNAL_DATA_PATH"dumps/dump_interopdb_15-06-11/interop/"
 #host and port
 MONGO_HOST="--host localhost --port 32020"
 #database name
@@ -25,19 +25,19 @@ echo "current path is:"$CURRENTPATH
 echo "rebuild db local ( restore from a dump)"
 echo "drop database"
 mongo $MONGO_HOST  $MONGO_PARAMETERS $MONGO_DB --eval "db.dropDatabase()"
-echo "restore a db in prod into new db"
+echo "restore a db prod-dump into new db"
 mongorestore $MONGO_HOST  $MONGO_PARAMETERS --db $MONGO_DB $DUMP_FILE
-
 echo "Save - Fin de restauration"
 echo "add data for selenium tests for all"
-
 mongo $MONGO_HOST $MONGO_PARAMETERS $MONGO_DB ./db_init.js
+echo "reapply mongo id update"
 
 java -jar selenium-server-standalone-2.44.0.jar -log selenium_vsc.log -htmlSuite "*googlechrome" "http://localhost/ebiobanques" $CURRENTPATH"/dev/testsSuites/visiteSiteSansConnexionTestSuite.html" $CURRENTPATH"/results_visite_sans_connexion.html"
 
 java -jar selenium-server-standalone-2.44.0.jar -log selenium_adm.log -htmlSuite "*googlechrome" "http://localhost/ebiobanques" $CURRENTPATH"/dev/testsSuites/administrationTestSuite.html" $CURRENTPATH"/results_administration.html"
 
 java -jar selenium-server-standalone-2.44.0.jar -log selenium_mbb.log -htmlSuite "*googlechrome" "http://localhost/ebiobanques" $CURRENTPATH"/dev/testsSuites/managerBiobankTestSuite.html" $CURRENTPATH"/results_manager_biobanque.html"
-echo "rebuild db local ( restore from a dump)"
-#sh $EXTERNAL_DATA_PATH"restore_mongo_from_dump.sh"
+
+java -jar selenium-server-standalone-2.44.0.jar -log selenium_mbb.log -htmlSuite "*googlechrome" "http://localhost/ebiobanques" $CURRENTPATH"/dev/testsSuites/simpleUserTestSuite.html" $CURRENTPATH"/results_simple_user.html"
+
 echo "END SCRIPT"

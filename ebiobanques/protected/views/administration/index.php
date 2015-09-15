60f10ad6850f7a0d49446e09222dc48bb5df0057
mpenicaud <matthieu.penicaud@inserm.fr>
2015-09-15 12:47:30 +0200

    implements administartion index page

--- ebiobanques/protected/views/administration/index.php
+++ ebiobanques/protected/views/administration/index.php
@@ -1,1 +1,11 @@
-Page d administration
+<h1>Administration</h1>
+<div class='help'>
+    <div class="help-title">
+        Administration de l'application
+    </div>
+    <div class="help-content">
+        <p> Vous pouvez dans cette section administrer les échantillons et les utilisateurs présents en base de données, et consulter les modifications faites sur la base.
+
+        </p>
+    </div>
+</div>

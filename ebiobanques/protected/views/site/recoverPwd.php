1a50e6ea190d0c503e4838835b03b103368363ed
mpenicaud <matthieu.penicaud@inserm.fr>
2016-02-02 13:46:47 +0100

    fix issue #50 : add explanation paragraph. Previous behavior was already the waited one.

--- ebiobanques/protected/views/site/recoverPwd.php
+++ ebiobanques/protected/views/site/recoverPwd.php
@@ -2,7 +2,10 @@
 /* @var $this SiteController */
 ?>
 <h1><?php echo Yii::t('common', 'forgotedPwd'); ?></h1>
-
+<div class ="help">
+    <div class="help-title">Récupération de mot de passe</div>
+    <div class='help-content'>Entrez votre login ou votre email. Votre mot de passe vous sera envoyé sur l'email utilisé lors de votre inscription.</div>
+</div>
 <div class="form">
     <?php
     $form = $this->beginWidget('CActiveForm', array(

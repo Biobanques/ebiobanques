fb91a4fa48c44231c77ee8397df3051fdf123542
nicolas malservet <nicolas@malservet.eu>
2016-08-19 16:01:31 +0200

    version 1.8.2 minor updates ( labels, i18n)

--- ebiobanques/themes/bootstrap/views/layouts/tpl_footer.php
+++ ebiobanques/themes/bootstrap/views/layouts/tpl_footer.php
@@ -1,6 +1,6 @@
 <footer>
     <div style="text-align:center;background-color: white;">
-        Copyright Inserm - Version 1.8.1 - Project Biobanques <a href="http://www.biobanques.eu">www.biobanques.eu</a>
+        Copyright Inserm - Version 1.8.2 - Project Biobanques <a href="http://www.biobanques.eu">www.biobanques.eu</a>
     </div>
 </footer>
 

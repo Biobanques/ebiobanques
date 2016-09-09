a47f4eccb34e03f699fdc29c5ea43a0d90a5b136
nicolas malservet <nicolas@malservet.eu>
2016-09-09 12:02:14 +0200

    [MANTIS 55] fix text displaying . Add boostrap encapsulation for divs in home page

--- ebiobanques/protected/views/site/accueil.php
+++ ebiobanques/protected/views/site/accueil.php
@@ -5,12 +5,14 @@
 ?>
 <div>
     <h3><?php echo Yii::t('common', 'indexTitle') ?></h3>
-    <div style="float:left;width:400px;font-size: 1.2em;text-align:justify;">&emsp;&emsp;<?php echo Yii::t('common', 'indexContent_p1') ?>
-    </div>
-    <div style="float:right;width:500px;">
-        <?php
-        echo CHtml::image(Yii::app()->baseUrl . "/images/picture_researcher.jpg", "picture researcher", array("width" => "500", "height" => "300"));
-        ?>
+    <div class="row">
+        <div class="col-lg-6" style=";font-size: 1.2em;text-align:justify;">&emsp;&emsp;<?php echo Yii::t('common', 'indexContent_p1') ?>
+        </div>
+        <div class="col-lg-6" >
+            <?php
+            echo CHtml::image(Yii::app()->baseUrl . "/images/picture_researcher.jpg", "picture researcher", array("width" => "500", "height" => "300"));
+            ?>
+        </div>
     </div>
     <div style="clear:both;"/>
 </div>

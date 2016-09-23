718ef65065c48e564dd877092f79fad56772fd01
mpenicaud <matthieu.penicaud@inserm.fr>
2016-09-23 10:48:57 +0200

    circleCi test

--- ebiobanques/CommonPropertiesTest.php
+++ ebiobanques/CommonPropertiesTest.php
@@ -45,6 +45,6 @@ class CommonProperties
     public static $IN_MAINTENANCE = false;
     public static $GMAPS_KEY = "";
     public static $TESTBROWSER = '';
-    public static $LAUNCHSELENIUM = false;
+    public static $LAUNCHSELENIUM = true;
     public static $SERVERTESTURL = 'http://ebiobanques.local';
 }
